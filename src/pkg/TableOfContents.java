5b73d500e7169540a29481a6bcf8c448089922f3
David-Nevezi-Strango <92172532+David-Nevezi-Strango@users.noreply.github.com>
2022-11-16 09:12:49 +0200

    lab7

--- src/pkg/TableOfContents.java
+++ src/pkg/TableOfContents.java
@@ -1,13 +1,25 @@
 package pkg;
 
+import java.util.ArrayList;
+
 public class TableOfContents implements Element{
+	ArrayList<String> contents;
+	
+	
+	public TableOfContents() {
+		this.contents = new ArrayList<String>();
+	}
 
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
+		contents.forEach(System.out::println);
 		
 	}
 
+	public void addEntry(String entry) {
+		contents.add(entry);
+	}
+	
 	@Override
 	public void add(Element element) {
 		// TODO Auto-generated method stub
